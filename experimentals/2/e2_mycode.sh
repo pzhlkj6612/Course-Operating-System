7937f924689b67e8f77647231915a199eb42ae48
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2018-12-23 19:34:48 +0800

    Update e2_mycode.sh

--- experimentals/2/e2_mycode.sh
+++ experimentals/2/e2_mycode.sh
@@ -1,5 +1,22 @@
 #!/bin/bash 
 
-echo "__a_Wqqq" > f1;
+echo -n "Enter your string: "
+read input;
+echo -n "$input" > f1;
+echo -n "Enter the character you want to find and replace: ";
+read charBeReplaced;
+echo -n "Enter the character that will be used for override: ";
+read charNew;
+echo -n "Enter the number of repetitions: ";
+read repetition;
+echo -n "Enter the time of replacement: ";
+read replacement_time;
+ls -l f1;
+od -c f1;
 gcc e2_mycode.c -o e2_mycode;
-./e2_mycode;
+./e2_mycode $charBeReplaced $charNew $repetition $replacement_time;
+ls -l f1;
+od -c f1;
+
+#echo "Show log:";
+#cat log;
